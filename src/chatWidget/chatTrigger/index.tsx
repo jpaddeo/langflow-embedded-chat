31025ba6d9a6c67e19766665fa08f108d16b07c9
Juan Pablo Addeo <jpaddeo@gmail.com>
2025-05-15 16:59:42 -0300

    feat: :zap: add trigger images props

--- src/chatWidget/chatTrigger/index.tsx
+++ src/chatWidget/chatTrigger/index.tsx
@@ -1,15 +1,50 @@
-import { MessageSquare, X } from "lucide-react"
-export default function ChatTrigger({ style, open, setOpen, triggerRef }: { style?: React.CSSProperties, open: boolean, setOpen: Function, triggerRef: React.RefObject<HTMLButtonElement> | null }) {
-
-    return (
-        <button ref={triggerRef} style={style}
-            onClick={() => { setOpen(!open) }}
-            onMouseDown={(e) => {
-                e.preventDefault()
-            }}
-            className="cl-trigger">
-            <X className={"cl-trigger-icon " + (open ? "cl-scale-100" : "cl-scale-0")} />
-            <MessageSquare className={"cl-trigger-icon " + (open ? "cl-scale-0" : "cl-scale-100")} />
-        </button>
-    )
+import { MessageSquare, X } from 'lucide-react';
+export default function ChatTrigger(
+  {
+    style,
+    open,
+    setOpen,
+    triggerRef,
+  }: {
+    style?: React.CSSProperties;
+    open: boolean;
+    setOpen: Function;
+    triggerRef: React.RefObject<HTMLButtonElement> | null;
+  },
+  triggerImages?: {
+    open: { [key: string]: string };
+    close: { [key: string]: string };
+  }
+) {
+  return (
+    <button
+      ref={triggerRef}
+      style={style}
+      onClick={() => {
+        setOpen(!open);
+      }}
+      onMouseDown={(e) => {
+        e.preventDefault();
+      }}
+      className='cl-trigger'
+    >
+      <X
+        className={'cl-trigger-icon ' + (open ? 'cl-scale-100' : 'cl-scale-0')}
+      />
+      {triggerImages?.open ? (
+        <img
+          src={triggerImages.open.url}
+          alt={triggerImages.open.alt}
+          width={triggerImages.open.width}
+          height={triggerImages.open.height}
+        />
+      ) : (
+        <MessageSquare
+          className={
+            'cl-trigger-icon ' + (open ? 'cl-scale-0' : 'cl-scale-100')
+          }
+        />
+      )}
+    </button>
+  );
 }
